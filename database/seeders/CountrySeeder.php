3b845413c29aac3612eda71e6d7520c9fa63180e
armandkarter <armandkarter@gmail.com>
2026-02-22 17:31:02 +0100

    ca concerne le country seeder

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -12,17 +12,17 @@ class CountrySeeder extends Seeder
     {
         $countries = [
             ['name' => 'France', 'image' => 'https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=1200'],
-            ['name' => 'Espagne', 'image' => 'https://images.unsplash.com/photo-1543783232-47502cdd839a?q=80&w=1200'],
-            ['name' => 'Italie', 'image' => 'https://images.unsplash.com/photo-1523906834658-6e24ef2386f9?q=80&w=1200'],
-            ['name' => 'Grèce', 'image' => 'https://images.unsplash.com/photo-1533105079780-92b9be482077?q=80&w=1200'],
+            ['name' => 'Spain', 'image' => 'https://images.unsplash.com/photo-1509840841025-9088ba78a826?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'], 
+            ['name' => 'Italy', 'image' => 'https://images.unsplash.com/photo-1523906834658-6e24ef2386f9?q=80&w=1200'],
+            ['name' => 'Greece', 'image' => 'https://images.unsplash.com/photo-1533105079780-92b9be482077?q=80&w=1200'],
             ['name' => 'Portugal', 'image' => 'https://images.unsplash.com/photo-1555881400-74d7acaacd8b?q=80&w=1200'],
-            ['name' => 'Croatie', 'image' => 'https://images.unsplash.com/photo-1512453979798-5ea266f8880c?q=80&w=1200'],
-            ['name' => 'Turquie', 'image' => 'https://images.unsplash.com/photo-1524231757912-21f4fe3a7200?q=80&w=1200'],
-            ['name' => 'Pays Bas', 'image' => 'https://images.unsplash.com/photo-1512470876302-972faa2aa9a4?q=80&w=1200'],
-            ['name' => 'Angleterre', 'image' => 'https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?q=80&w=1200'],
-            ['name' => 'Estonie', 'image' => 'https://images.unsplash.com/photo-1548834181-110bbd4ca490?q=80&w=1200'],
-            ['name' => 'Allemagne', 'image' => 'https://images.unsplash.com/photo-1467269204594-9661b134dd2b?q=80&w=1200'],
-            ['name' => 'Chine', 'image' => 'https://images.unsplash.com/photo-1508804185872-d7badad00f7d?q=80&w=1200'],
+            ['name' => 'Croatia', 'image' => 'https://images.unsplash.com/photo-1504019347908-b45f9b0b8dd5?q=80&w=1200'],
+            ['name' => 'Turkey', 'image' => 'https://images.unsplash.com/photo-1524231757912-21f4fe3a7200?q=80&w=1200'],
+            ['name' => 'Netherlands', 'image' => 'https://images.unsplash.com/photo-1512470876302-972faa2aa9a4?q=80&w=1200'],
+            ['name' => 'England', 'image' => 'https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?q=80&w=1200'],
+            ['name' => 'Estonia', 'image' => 'https://images.unsplash.com/photo-1593103415082-969472658826?q=80&w=1200'],   
+            ['name' => 'Germany', 'image' => 'https://images.unsplash.com/photo-1467269204594-9661b134dd2b?q=80&w=1200'],
+            ['name' => 'China', 'image' => 'https://images.unsplash.com/photo-1508804185872-d7badad00f7d?q=80&w=1200'],
         ];
 
         foreach ($countries as $country) {
